8fcca9fe83080f121c6b304ef9e3f45373705d75
nosianuri <nurinosia@gmail.com>
2022-11-27 00:55:59 +0600

    Set Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
-import Banner from './components/Banner/Banner';
-import Blog from './components/Blog/Blog';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Services from './components/Services/Services';
-import Testimonial from './components/Testimonial/Testimonial';
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <Banner />
-      <Services />
-      <About />
-      <Blog />
-      <Testimonial />
-      <Footer />
-    </div>
+    <>
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </>
   );
 }
 
