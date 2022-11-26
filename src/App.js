9872cbcec14ec48379618198716fe41a5987ec02
nosianuri <nurinosia@gmail.com>
2022-11-27 01:20:34 +0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './header.css';
 import logo from '../../assets/logo.png';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 class Header extends Component {
   state = { clicked: false };
@@ -10,30 +12,21 @@ class Header extends Component {
   }
   render() {
     return (
-      <div className='header'>
-        <nav>
-          <a href="/">
-            <img className='logo' src={logo} alt="Logo" />
-          </a>
-
-          
-            <ul id='navbar' className={this.state.clicked ? "#navbar active" : "#navbar"}>
-              <li>
-                <a className='active' href="/">ABOUT US</a></li>
-              <li><a href="/todo">CONTACT US</a></li>
-              <li><a href="/calender">PRIVACY POLICY</a></li>
-              <li><a href="/tasks">TERMS & CONDITION</a></li>
-              <button className='phone-buton'><a href="/phone" className='phone'><i class="fa-solid fa-phone" /> (855) 939-0621</a></button>
-              
-            </ul>
-          
-
-          <div id='mobile' onClick={this.handleClick}>
-            <i id='bar' className={this.state.clicked ? "fa-solid fa-times" : "fas fa-bars"}></i>
-
-          </div>
-        </nav>
-      </div>
+      <Navbar>
+      <Container>
+        <Navbar.Brand href="#home"><img
+              alt=""
+              src={logo}
+              className="d-inline-block align-top"
+            />{' '}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text> <h2>Over $1 Billion Recovered</h2>
+            <p>in Verdicts & Settlements*</p>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
   }
 
