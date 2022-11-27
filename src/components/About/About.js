9eb850aeb02ca30a74775a00aa0c6b507e775c44
nosianuri <nurinosia@gmail.com>
2022-11-27 22:46:26 +0600

    Update about & blog section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
 
                     <div className='col-md-6'>
                         <div class="container">
-                            <h2 class="card-title">CAN I CLAIM COMPENSATION FOR TOXIC WATER RELATED ILLNESSES AT CAMP LEJEUNE?</h2>
+                            <h2 class="card-title">CAN I CLAIM <br /> COMPENSATION FOR <br /> TOXIC WATER RELATED <br /> ILLNESSES AT CAMP <br /> LEJEUNE?</h2>
                             <p class="card-text">Water at the Marine Corps camp in Jacksonville, North Carolina, was found to have been contaminated with benzene, trichloroethylene and perchloroethylene among others between 1953 to 1987. In the time since, residents of the base have experienced severe negative side-effects, like Cancer, Renal Toxicity, Infertitlity, Parkinson’s, Myeloma and many more.</p>
                             <div className='card-mute'>
                                 <p class="card-text"><small class="text-muted">The U.S. Department of Veteran Affairs (VA) tried to avoid helping US Marines and their families, but a new bipartisan bill allows our heroes to get the help they deserve. Our specialist lawyers can help you and your family get the maximum compensation you deserve.</small></p>
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -23,8 +23,8 @@ const Blog = () => {
                                         <p>The easiest way to find out and start the process is to answer a few simple questions via our online form. We can check today if you are eligble to receive a settlement figure for your suffering.</p>
                                     </div>
                                 </div>
-                                </div>
-                                <div className='col-md-4'>
+                            </div>
+                            <div className='col-md-4'>
                                 <div className='card-blog'>
                                     <div className='imgbox'>
                                         <img src={img2} alt="" />
@@ -35,8 +35,8 @@ const Blog = () => {
                                         <p>Our attorneys offer a free initial chat to discuss your illmesses and eligibility. They can then assess how much compensation you should be owed, taking into account medical bills etc.</p>
                                     </div>
                                 </div>
-                                </div>
-                                <div className='col-md-4'>
+                            </div>
+                            <div className='col-md-4'>
                                 <div className='card-blog'>
                                     <div className='imgbox'>
                                         <img src={img3} alt="" />
@@ -47,11 +47,11 @@ const Blog = () => {
                                         <p>We believe everyone that has been affected is due the maximum compensation for their suffering. With all the information and your authority we ensure we get you the maximum settlement you deserve.</p>
                                     </div>
                                 </div>
-                                </div>
-                            
+                            </div>
+
                         </div>
                     </div>
-                   
+
                 </div>
                 <div className='get-buton'><button>GET FREE QUOTE</button></div>
 
