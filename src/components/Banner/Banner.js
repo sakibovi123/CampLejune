be80a98cbb5d3fb95246d915c63dd0690d9b4c50
sakibovi123 <sakibovi123@gmail.com>
2022-11-27 22:14:41 +0600

    form added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,51 +5,56 @@ import './banner.css';
 
 
 const Banner = () => {
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    description: '',
-    country: 'canada',
-    agree: false,
-    gender: 'Other'
-  });
 
-  const [error, setError] = useState({
-    name: ''
-  });
+  let sendDataToLeadProsper = async (e) => {
+    e.preventDefault()
+    let responseLeadprosper = await fetch("https://api.leadprosper.io/ingest", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "lp_campaign_id": "10056",
+        "lp_supplier_id": "21039",
+        "lp_key": "xzmjar7ns7ppq",
+        "first_name": e.target.first_name.value,
+        "last_name": e.target.last_name.value,
+        "phone": e.target.phone.value,
+        "email": e.target.email.value,
+        "camp_lj": e.target.camp_lj.value,
+        "comment": e.target.comment.value,
+        "representation": e.target.representation.value,
+        "lp_action": "test"
+      })
+    })
 
-  const onChange = (e) => {
-    const { value, name, type, checked } = e.target;
+    let responseToZapier = await fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "lp_campaign_id": "10056",
+        "lp_supplier_id": "21039",
+        "lp_key": "xzmjar7ns7ppq",
+        "first_name": e.target.first_name.value,
+        "last_name": e.target.last_name.value,
+        "phone": e.target.phone.value,
+        "email": e.target.email.value,
+        "camp_lj": e.target.camp_lj.value,
+        "comment": e.target.comment.value,
+        "representation": e.target.representation.value,
+        "lp_action": "test"
+      })
+    })
 
-    setForm((state) => ({
-      ...state,
-      [name]: type === 'checkbox' ? checked : value
-    }));
+    let data1 = responseLeadprosper.json()
+    let data2 = responseToZapier.json()
+    console.log(responseLeadprosper, data1)
+    console.log(data2, responseToZapier)
+
   }
 
-  const showData = () => {
-    console.log('Form: ', form);
-
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (form.name.length < 5) {
-      setError((state) => ({
-        ...state,
-        name: 'Too short'
-      }));
-      return;
-    } else {
-      setError((state) => ({
-        ...state,
-        name: ''
-      }));
-    };
-    showData();
-    
-  }
 
   return (
     <div>
@@ -74,62 +79,44 @@ const Banner = () => {
           {/* *****************banner form*************** */}
 
           <Col xs={12} md={6} xl={5}>
-          <div className='banner-form'>
-            <form onSubmit={onSubmit}>
+          <div className='banner-form form-group'>
+            <form onSubmit={sendDataToLeadProsper}>
+              
+              <label htmlFor="">Did you or a loved one serve, live, or work at Camp Lejeune for at least 30 days between 1953 and 1987?</label><br/>
+              <select name="camp_lj" id="" className="form-control">
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select><br/>
 
-              <label>Name:
-                <input onchange={onChange} name='name' value={form.name} />
-              </label>
-              {!!error.name && (
-                <div>
-                  <i>{error.name}</i>
-                </div>
-              )}
-              <hr />
+              <label htmlFor="first_name">First Name</label><br/>
+              <input className="form-control" type="text" name="first_name" placeholder="Enter First Name..."/><br/>
 
-              <label>Email:
-                <input onchange={onChange} name='email' value={form.email} />
-              </label>
+              <label htmlFor="first_name">Last Name</label><br/>
+              <input className="form-control" type="text" name="last_name" placeholder="Enter Last Name..."/><br/>
 
-              <hr />
+              <label htmlFor="phone">Phone</label><br/>
+              <input className="form-control" type="text" name="phone" placeholder="Enter Phone..."/><br/>
 
-              <label>Description:
-                <input onchange={onChange} name='email' value={form.description} />
-              </label>
+              <label htmlFor="email">Email</label><br/>
+              <input className="form-control" type="email" name="email" placeholder="Enter Email..."/><br/>
 
-              <hr />
+              <label htmlFor="representation">Do you already have an attorney representing you for this claim?</label><br/>
+              <select name="representation" className="form-control">
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select><br/>
 
-              <label>
-                Country:
-                <select onchange={onChange} value={form.country} name="country">
-                  <option value="us">U.S.</option>
-                  <option value="us">Canada</option>
-                  <option value="us">Ukraine</option>
-                </select>
-              </label>
+              <label htmlFor="representation">Do you already have an attorney representing you for this claim?</label><br/>
+              <select name="representation" className="form-control">
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select><br/>
 
-              <hr />
+              <label htmlFor="Briefly describe what happened:">Briefly describe what happened:</label><br/>
+              <textarea name="comment" id="" cols="30" rows="10" className="form-control" placeholder="Briefly Describe what happened"></textarea><br/>
 
-              <label>
-                Gender:
-                <div className='radio'>
-                  <input onchange={onChange} type="radio" value="Male" name='gender' checked={form.gender === 'Male'} />Male
-                  <input onchange={onChange} type="radio" value="Female" name='gender' checked={form.gender === 'Female'} />Female
-                  <input onchange={onChange} type="radio" value="Other" name='gender' checked={form.gender === 'Other'} />Other
-                </div>
-              </label>
-
-              <hr />
-
-              <label>
-                Agree:
-                <input type="checkbox" onChange={onChange} name="agree" value={form.agree} />
-              </label>
-
-              <div>
-                <button className='form-submit' >Submit</button>
-              </div>
-
+              <button className="btn btn-outline-primary">Send Request</button>
+  
             </form>
             </div>
           </Col>
