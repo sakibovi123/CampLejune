775d3865e0cdf7c1311643e0812f6119c382291f
nosianuri <nurinosia@gmail.com>
2022-12-09 00:35:40 +0600

    Update meta Content

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -5,18 +5,13 @@ import img2 from '../../assets/beverlyhill-bar-association.webp';
 import img3 from '../../assets/consumer-attorneys-association-of-los-angeles.webp';
 import img4 from '../../assets/consumer-attorneys-of-california.webp';
 import img5 from '../../assets/los-angeles-county-bar-association.webp';
-import img6 from '../../assets/Super-Lawyers.webp'; 
+import img6 from '../../assets/Super-Lawyers.webp';
 import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const Journal = () => {
   return (
     <div className='journal'>
-      {/* <img src={img1} alt="journal" />
-      <img src={img2} alt="journal1" />
-      <img src={img3} alt="journal2" />
-      <img src={img4} alt="journal3" /> */}
-
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -24,41 +19,38 @@ const Journal = () => {
           delay: 5000,
           disableOnInteraction: false,
         }}
-
-
         modules={[Autoplay]}
         className="mySwiper"
       >
         <SwiperSlide><div className='journal'>
-        <img src={img1} alt="journal" />
-        <img src={img2} alt="journal1" />
-        <img src={img3} alt="journal2" />
-        <img src={img4} alt="journal3" />
-        <img src={img5} alt="journal3" />
-        <img src={img6} alt="journal3" />
-
-    </div></SwiperSlide>
+          <img src={img1} alt="journal" />
+          <img src={img2} alt="journal1" />
+          <img src={img3} alt="journal2" />
+          <img src={img4} alt="journal3" />
+          <img src={img5} alt="journal3" />
+          <img src={img6} alt="journal3" />
+        </div></SwiperSlide>
         <SwiperSlide><div className='journal'>
-        <img src={img1} alt="journal" />
-        <img src={img2} alt="journal1" />
-        <img src={img3} alt="journal2" />
-        <img src={img4} alt="journal3" />
-        <img src={img5} alt="journal3" />
-        <img src={img6} alt="journal3" />
-    </div></SwiperSlide>
+          <img src={img1} alt="journal" />
+          <img src={img2} alt="journal1" />
+          <img src={img3} alt="journal2" />
+          <img src={img4} alt="journal3" />
+          <img src={img5} alt="journal3" />
+          <img src={img6} alt="journal3" />
+        </div></SwiperSlide>
         <SwiperSlide><div className='journal'>
-        <img src={img1} alt="journal" />
-        <img src={img2} alt="journal1" />
-        <img src={img3} alt="journal2" />
-        <img src={img4} alt="journal3" />
-        <img src={img5} alt="journal3" />
-        <img src={img6} alt="journal3" />
-    </div></SwiperSlide>
+          <img src={img1} alt="journal" />
+          <img src={img2} alt="journal1" />
+          <img src={img3} alt="journal2" />
+          <img src={img4} alt="journal3" />
+          <img src={img5} alt="journal3" />
+          <img src={img6} alt="journal3" />
+        </div></SwiperSlide>
 
       </Swiper>
 
     </div>
-   
+
   )
 }
 
